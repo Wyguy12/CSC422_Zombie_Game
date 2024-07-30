739ad9fe54127aad3f6b3ae5852f0b1d75bf2bcf
NCzaplewski <natasha.czaplewski@gmail.com>
2024-07-29 20:19:58 -0500

    Update Character.java
    Add code for character class.

--- src/main/java/com/csc422_group_project/zombiewargame/Models/Character.java
+++ src/main/java/com/csc422_group_project/zombiewargame/Models/Character.java
@@ -9,5 +9,31 @@ package com.csc422_group_project.zombiewargame.Models;
  * @author 
  */
 public class Character {
-    
+    protected int healthBar;
+    protected int attackPower;
+
+    //Constructor
+    public Character(int healthBar, int attackPower) {
+        this.healthBar = healthBar;
+        this.attackPower = attackPower;
+    }
+
+    //Damage opponent
+    public void attack(Character target) {
+        target.receiveDamage(this.attackPower);
+    }
+
+    //Receive damage
+    public void receiveDamage(int damage) {
+        this.healthBar -= damage;
+    }
+
+    //Check if Character is alive
+    public boolean isAlive() {
+        if (this.healthBar > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
