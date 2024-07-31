0afede0bc7665a8d81da2feec4dbe246bcf3d3a7
Wyatt <139176750+Wyguy12@users.noreply.github.com>
2024-07-30 21:13:37 -0500

    Update Survivor.java

--- src/main/java/com/csc422_group_project/zombiewargame/Models/SurvivorTypes/Survivor.java
+++ src/main/java/com/csc422_group_project/zombiewargame/Models/SurvivorTypes/Survivor.java
@@ -7,9 +7,30 @@ package com.csc422_group_project.zombiewargame.Models.SurvivorTypes;
 import com.csc422_group_project.zombiewargame.Models.Character;
 
 /**
- *
- * @author Wyatt
- */
+ * Abstract class for the survivor
+ * @author Wyatt 
+ */ 
 public abstract class Survivor extends Character {
-    
+    protected String survivorType;
+
+
+    /**
+     * Constructor for Survivor
+     * @param healthBar The health of the survivor
+     * @param attackPower The attack power of the survivor
+     * @param survivorType The type of survivor
+     */
+    public Survivor(int healthBar, int attackPower, String survivorType) {
+        super(healthBar, attackPower); // Calls the constructor of the parent class
+        this.survivorType = survivorType; // Sets the survivor type
+    }   
+
+
+    /**
+     * Gets the type of survivor
+     * @return The type of survivor
+     */
+    public String getSurvivorType() {
+        return survivorType; // Returns the survivor type
+    }
 }
